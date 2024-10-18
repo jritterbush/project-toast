d86b573966a48967d2cfe0eb3c6f29acd06b0c6c
John Ritterbush <jritterbush@gmail.com>
2024-10-18 16:25:52 -0500

    refactor to use context/provider

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([]);
+
+  const addToast = (message, variant) => {
+    const id = crypto.randomUUID();
+    setToasts((prevToasts) => [...prevToasts, { id, message, variant }]);
+  };
+
+  const removeToast = (id) => {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
